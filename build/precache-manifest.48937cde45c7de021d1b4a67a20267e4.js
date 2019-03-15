d8a2272e36def4ea1c8b31c195fde7cd3f173910
Sergey Chuprina <sergey.chuprina@hyuna.bb>
2019-03-15 17:15:01 +0200

    build added

---
+++ build/precache-manifest.48937cde45c7de021d1b4a67a20267e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9af88b85e3ca62a79056",
+    "url": "/static/js/main.5b446c77.chunk.js"
+  },
+  {
+    "revision": "929b1fa7bd8058acaf33",
+    "url": "/static/js/2.9eaa3743.chunk.js"
+  },
+  {
+    "revision": "9af88b85e3ca62a79056",
+    "url": "/static/css/main.cf287c0f.chunk.css"
+  },
+  {
+    "revision": "b3fe21ba04102223f4cd3e93028b3d47",
+    "url": "/index.html"
+  }
+];
